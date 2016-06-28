1919d10b45a2cf25750ab01ec527731f133414c2
Ryan Owens <RyanOwens@linux.com>
2016-06-28 15:01:10 -0500

    Changed login/signup buttons

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,13 @@
-<?php
+<?php Namespace Ryanvade\LoginRedirect;
 
-return function () {
-    echo 'Hello, world!';
+use Flarum\Event\ConfigureClientView;
+use Illuminate\Contracts\Events\Dispatcher;
+
+return function (Dispatcher $events) {
+$events->listen(ConfigureClientView::class, function (ConfigureClientView $event) {
+    if ($event->isForum()) {
+        $event->addAssets(__DIR__.'/js/forum/dist/extension.js');
+        $event->addBootstrapper('ryanvade/flarum-login-redirect/main');
+    }
+});
 };
--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -1,7 +1,14 @@
-import SignUpModal from 'flarum/components/SignUpModal'
+import { extend } from 'flarum/extend';
+import SignUpModal from 'flarum/components/SignUpModal';
+import HeaderSecondary from 'flarum/components/HeaderSecondary';
 
 app.initializers.add('ryanvade-flarum-login-redirect', function() {
-  extend(SignUpModal.prototype, 'view', function(vdom) {
-    console.log(vdom);
+  console.log("Hello");
+  extend(HeaderSecondary.prototype, 'items', function(items) {
+    items.remove('logIn');
+    items.remove('signUp');
+    items.remove('logOut');
+    items.add('SignUp', <a href="http://localhost:8888/auth/register" className="Button Button--link">Sign Up</a>);
+    items.add('LogIn', <a href="http://localhost:8888/auth/login" className="Button Button--link">Log In</a>);
   });
 });
