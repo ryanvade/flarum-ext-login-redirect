05e7e193b7fb9ece713a064ca023e87f69077aa7
ryanvade <ryanvader82@gmail.com>
2016-06-30 16:38:00 -0500

    Changed URL and added README.md

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -11,13 +11,13 @@ app.initializers.add('ryanvade-flarum-login-redirect', function() {
     if(!items.has('session'))
     {
       console.log('Does not have session');
-      items.add('SignUp', <a href="http://localhost:8888/auth/register" className="Button Button--link">Sign Up</a>);
-      items.add('LogIn', <a href="http://localhost:8888/auth/login" className="Button Button--link">Log In</a>);
+      items.add('SignUp', <a href="http://localhost/auth/register" className="Button Button--link">Sign Up</a>);
+      items.add('LogIn', <a href="http://localhost/auth/login" className="Button Button--link">Log In</a>);
     }
   });
 
   extend(SessionDropdown.prototype, 'items', function(items){
     items.remove('logOut');
-    items.add('LogOut', <a href="http://localhost:8888/auth/logout" className="icon fa fa-fw fa-sign-out Button-icon">Log Out</a>)
+    items.add('LogOut', <a href="http://localhost/auth/logout" className="icon fa fa-fw fa-sign-out Button-icon">Log Out</a>)
   });
 });
