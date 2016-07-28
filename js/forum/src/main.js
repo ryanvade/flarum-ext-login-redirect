d5bc864fe06ad9fdef2e4cd7769fd0f26a327afb
Ryan Owens <RyanOwens@linux.com>
2016-07-28 15:39:00 -0500

    Attempting to modify the Modal

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -2,22 +2,37 @@ import { extend } from 'flarum/extend';
 import SignUpModal from 'flarum/components/SignUpModal';
 import HeaderSecondary from 'flarum/components/HeaderSecondary';
 import SessionDropdown from 'flarum/components/SessionDropdown';
+import LogInModal from 'flarum/components/LogInModal';
 
 app.initializers.add('ryanvade-flarum-login-redirect', function() {
-  console.log("Hello");
   extend(HeaderSecondary.prototype, 'items', function(items) {
     items.remove('logIn');
     items.remove('signUp');
     if(!items.has('session'))
     {
       console.log('Does not have session');
-      items.add('SignUp', <a href="http://localhost:8888/auth/register" className="Button Button--link">Sign Up</a>);
-      items.add('LogIn', <a href="http://localhost:8888/auth/login" className="Button Button--link">Log In</a>);
+      items.add('SignUp', <a href="http://localhost/auth/register" className="Button Button--link">Sign Up</a>);
+      items.add('LogIn', <a href="http://localhost/auth/login" className="Button Button--link">Log In</a>);
+    }else
+    {
+      console.log('Has Session');
     }
   });
 
   extend(SessionDropdown.prototype, 'items', function(items){
     items.remove('logOut');
-    items.add('LogOut', <a href="http://localhost:8888/auth/logout" className="icon fa fa-fw fa-sign-out Button-icon">Log Out</a>)
+    items.add('LogOut', <a href="http://localhost/auth/logout" className="icon fa fa-fw fa-sign-out Button-icon">Log Out</a>)
   });
+
+  LogInModal.content = function() {
+    console.log("Hello");
+    return [
+      <div className="Modal-body">
+      <span>Hello</span>
+      </div>,
+      <div className="Modal-footer">
+        <span>Hello</span>
+      </div>
+    ];
+  };
 });
