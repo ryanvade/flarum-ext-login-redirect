e93f0028a40aa4e469ac267ea55cd16d56c02e5f
Ryan Owens <RyanOwens@linux.com>
2016-11-06 10:05:44 -0600

    redirect to cosmoquest.org

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -7,39 +7,35 @@ import LogInModal from 'flarum/components/LogInModal';
 import LogInButtons from 'flarum/components/LoginButtons';
 
 app.initializers.add('ryanvade-flarum-login-redirect', function() {
-  console.log("Hello");
   extend(HeaderSecondary.prototype, 'items', function(items) {
     items.remove('logIn');
     items.remove('signUp');
     if(!items.has('session'))
     {
       console.log('Does not have session');
-      items.add('SignUp', <a href="http://localhost/auth/register" className="Button Button--link">Sign Up</a>);
-      items.add('LogIn', <a href="http://localhost/auth/login" className="Button Button--link">Log In</a>);
-    }else
-    {
-      console.log('Has Session');
+      items.add('SignUp', <a href="https://cosmoquest.org/auth/register" className="Button Button--link">Sign Up</a>);
+      items.add('LogIn', <a href="https://cosmoquest.org/auth/login" className="Button Button--link">Log In</a>);
     }
   });
 
   extend(SessionDropdown.prototype, 'items', function(items){
     items.remove('logOut');
-    items.add('LogOut', <a href="http://localhost/auth/logout" className="icon fa fa-fw fa-sign-out Button-icon">Log Out</a>)
+    items.add('LogOut', <a href="https://cosmoquest.org/auth/logout" className="icon fa fa-fw fa-sign-out Button-icon">Log Out</a>)
   });
 
   LogInModal.prototype.content = function () {
     return [
       <div className="Modal-body">
         <div className="Form Form--centered">
-          <a className="btn btn-primary" href="http://localhost/auth/login">Login with Cosmoquest</a>
+          <a className="btn btn-primary" href="https://cosmoquest.org/auth/login">Login with Cosmoquest</a>
         </div>
        </div>,
       <div className="Modal-footer">
         <p className="LogInModal-forgotPassword">
-        <a className="btn" href="http://localhost/password/reset">Forgot Password?</a>
+        <a className="btn" href="https://cosmoquest.org/password/reset">Forgot Password?</a>
         </p>
         <p className="LogInModal-signUp">
-          <a className="btn" href="http://localhost/auth/register">Register with Cosmoquest</a>
+          <a className="btn" href="https://cosmoquest.org/auth/register">Register with Cosmoquest</a>
         </p>
       </div>
     ];
@@ -51,7 +47,7 @@ app.initializers.add('ryanvade-flarum-login-redirect', function() {
         <div className="Form Form--centered">
           <p className="helpText">Change your passsword.</p>
           <div className="Form-group">
-            <a href="/password/reset" class='Button Button--primary Button--block'>Click here to change your password.</a>
+            <a href="https://cosmoquest.org/password/reset" class='Button Button--primary Button--block'>Click here to change your password.</a>
           </div>
         </div>
       </div>
